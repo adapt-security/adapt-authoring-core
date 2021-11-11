dd1dd90ca792d1f2ad20522f6ec25faadb91b218
Tom Taylor <hello@tomtaylor.name>
2021-11-11 16:24:01 +0000

    Update for ESM

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-module.exports = {
-  AbstractModule: require('./lib/AbstractModule'),
-  App: require('./lib/App'),
-  DependencyLoader: require('./lib/DependencyLoader'),
-  Events: require('./lib/Events'),
-  Hook: require('./lib/Hook'),
-  Utils: require('./lib/Utils')
-};
+export { AbstractModule } from './lib/AbstractModule';
+export { App } from './lib/AbstractModule';
+export { DependencyLoader } from './lib/DependencyLoader';
+export { Events } from './lib/Events';
+export { Hook } from './lib/Hook';
+export { Utils }  from './lib/Utils';
