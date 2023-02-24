92236454ddf8ae98017afacdcd4bd2597d208b8d
Thomas Taylor <hello@tomtaylor.name>
2023-02-24 18:23:11 +0000

    Update doc comments

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/** 
+ * Core functionality
+ * @namespace core
+ */
 export { default as AbstractModule } from './lib/AbstractModule.js';
 export { default as App } from './lib/App.js';
 export { default as DependencyLoader } from './lib/DependencyLoader.js';
