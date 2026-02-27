38920b7fb96b18e1739044652166d2e13f52ac4e
Thomas Taylor <thomas.taylor@kineo.com>
2026-02-27 17:03:00 +0000

    Fix: Add HEAD fetch to only pull in valid badges

--- docs/plugins/coremodules.js
+++ docs/plugins/coremodules.js
@@ -1,20 +1,34 @@
+import fetch from 'node-fetch'
+
 export default class CoreModules {
   async run () {
     this.manualFile = 'coremodules.md'
     this.replace = {
       VERSION: this.app.pkg.version,
-      MODULES: this.generateMd()
+      MODULES: await this.generateMd()
     }
   }
 
-  generateMd () {
-    return Object.keys(this.app.dependencies).sort().reduce((s, name) => {
+  async getWorkflowBadges (homepage) {
+    if (!homepage) return []
+    const workflows = ['tests', 'standardjs']
+    const results = await Promise.all(workflows.map(async w => {
+      const url = `${homepage}/actions/workflows/${w}.yml`
+      try {
+        const res = await fetch(url, { method: 'HEAD', redirect: 'follow' })
+        if (res.ok) return `[![${w}](${url}/badge.svg)](${url})`
+      } catch {}
+      return null
+    }))
+    return results.filter(Boolean)
+  }
+
+  async generateMd () {
+    const rows = await Promise.all(Object.keys(this.app.dependencies).sort().map(async name => {
       const { version, description, homepage } = this.app.dependencies[name]
-      const workflows = ['tests', 'standardjs']
-      const badges = homepage
-        ? workflows.map(w => `[![${w}](${homepage}/actions/workflows/${w}.yml/badge.svg)](${homepage}/actions/workflows/${w}.yml)`).join('<br>')
-        : ''
-      return `${s}\n| ${homepage ? `[${name}](${homepage})` : name} | ${version} | ${description} | ${badges} |`
-    }, '| Name | Version | Description | Status |\n| - | :-: | - | :-: |')
+      const badges = await this.getWorkflowBadges(homepage)
+      return `| ${homepage ? `[${name}](${homepage})` : name} | ${version} | ${description} | ${badges.join('<br>')} |`
+    }))
+    return `| Name | Version | Description | Status |\n| - | :-: | - | :-: |\n${rows.join('\n')}`
   }
 }
