8bf56ea30fcbdc93a48fb6e630c6b422875fb8b5
Tom Taylor <thomas.taylor@kineo.com>
2026-03-10 10:21:44 +0000

    Reorder workflows array in getWorkflowBadges

--- docs/plugins/coremodules.js
+++ docs/plugins/coremodules.js
@@ -11,7 +11,7 @@ export default class CoreModules {
 
   async getWorkflowBadges (homepage) {
     if (!homepage) return []
-    const workflows = ['tests', 'standardjs', 'releases']
+    const workflows = ['standardjs', 'tests', 'releases']
     const results = await Promise.all(workflows.map(async w => {
       const url = `${homepage}/actions/workflows/${w}.yml`
       const badgeUrl = `${url}/badge.svg`
