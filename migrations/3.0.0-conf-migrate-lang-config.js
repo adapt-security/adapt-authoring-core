0db6a9db0668578850e5f9c314a295dee250d00e
Thomas Taylor <thomas.taylor@kineo.com>
2026-03-26 15:00:28 +0000

    Update: Simplify config migrations to use where/mutate DSL

--- migrations/3.0.0-conf-migrate-lang-config.js
+++ migrations/3.0.0-conf-migrate-lang-config.js
@@ -1,40 +1,19 @@
-import fs from 'fs/promises'
-import path from 'path'
-import { glob } from 'glob'
-
 export default function (migration) {
   migration.describe('Move adapt-authoring-lang config keys to adapt-authoring-core')
 
-  migration.run(async ({ appDir, dryRun, log }) => {
-    const files = await glob('conf/*.config.js', { cwd: appDir, absolute: true })
-    for (const filePath of files) {
-      const config = (await import(filePath)).default
+  migration
+    .where('adapt-authoring-lang')
+    .mutate(config => {
       const lang = config['adapt-authoring-lang']
-      if (!lang) continue
-
-      const core = config['adapt-authoring-core'] || {}
-      let changed = false
+      const core = config['adapt-authoring-core'] ||= {}
 
       if ('defaultLang' in lang) {
         core.defaultLang = lang.defaultLang
         delete lang.defaultLang
-        changed = true
-        log('info', `${path.basename(filePath)}: moved defaultLang to adapt-authoring-core`)
       }
 
-      if (!changed) continue
-
-      config['adapt-authoring-core'] = core
       if (!Object.keys(lang).length) {
         delete config['adapt-authoring-lang']
       }
-
-      const output = `export default ${JSON.stringify(config, null, 2)}\n`
-      if (dryRun) {
-        log('info', `[DRY RUN] would write ${filePath}`)
-        continue
-      }
-      await fs.writeFile(filePath, output, 'utf8')
-    }
-  })
+    })
 }
--- migrations/3.0.0-conf-migrate-logger-config.js
+++ migrations/3.0.0-conf-migrate-logger-config.js
@@ -1,56 +1,25 @@
-import fs from 'fs/promises'
-import path from 'path'
-import { glob } from 'glob'
-
 export default function (migration) {
   migration.describe('Move adapt-authoring-logger config keys to adapt-authoring-core')
 
-  migration.run(async ({ appDir, dryRun, log }) => {
-    const files = await glob('conf/*.config.js', { cwd: appDir, absolute: true })
-    for (const filePath of files) {
-      const config = (await import(filePath)).default
+  migration
+    .where('adapt-authoring-logger')
+    .mutate(config => {
       const logger = config['adapt-authoring-logger']
-      if (!logger) continue
-
-      const core = config['adapt-authoring-core'] || {}
-      let changed = false
+      const core = config['adapt-authoring-core'] ||= {}
 
       if ('levels' in logger) {
         core.logLevels = logger.levels
         delete logger.levels
-        changed = true
-        log('info', `${path.basename(filePath)}: moved levels → logLevels`)
       }
       if ('showTimestamp' in logger) {
         core.showLogTimestamp = logger.showTimestamp
         delete logger.showTimestamp
-        changed = true
-        log('info', `${path.basename(filePath)}: moved showTimestamp → showLogTimestamp`)
       }
-      if ('mute' in logger) {
-        delete logger.mute
-        changed = true
-        log('info', `${path.basename(filePath)}: removed mute (use empty logLevels array instead)`)
-      }
-      if ('dateFormat' in logger) {
-        delete logger.dateFormat
-        changed = true
-        log('info', `${path.basename(filePath)}: removed dateFormat`)
-      }
+      delete logger.mute
+      delete logger.dateFormat
 
-      if (!changed) continue
-
-      config['adapt-authoring-core'] = core
       if (!Object.keys(logger).length) {
         delete config['adapt-authoring-logger']
       }
-
-      const output = `export default ${JSON.stringify(config, null, 2)}\n`
-      if (dryRun) {
-        log('info', `[DRY RUN] would write ${filePath}`)
-        continue
-      }
-      await fs.writeFile(filePath, output, 'utf8')
-    }
-  })
+    })
 }
