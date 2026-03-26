77da6e2a8de99f2d10dde6a367fc3e0a63aaf96c
Thomas Taylor <thomas.taylor@kineo.com>
2026-03-26 15:22:40 +0000

    Update: Use replace/remove DSL in config migrations

--- migrations/3.0.0-conf-migrate-lang-config.js
+++ migrations/3.0.0-conf-migrate-lang-config.js
@@ -3,13 +3,5 @@ export default function (migration) {
 
   migration
     .where('adapt-authoring-lang')
-    .mutate(config => {
-      const lang = config['adapt-authoring-lang']
-      const core = config['adapt-authoring-core'] ||= {}
-
-      if (lang.defaultLang && !core.defaultLang) {
-        core.defaultLang = lang.defaultLang
-        delete config['adapt-authoring-lang']
-      }
-    })
+    .replace('defaultLang', 'adapt-authoring-core')
 }
--- migrations/3.0.0-conf-migrate-logger-config.js
+++ migrations/3.0.0-conf-migrate-logger-config.js
@@ -3,23 +3,7 @@ export default function (migration) {
 
   migration
     .where('adapt-authoring-logger')
-    .mutate(config => {
-      const logger = config['adapt-authoring-logger']
-      const core = config['adapt-authoring-core'] ||= {}
-
-      if ('levels' in logger) {
-        core.logLevels = logger.levels
-        delete logger.levels
-      }
-      if ('showTimestamp' in logger) {
-        core.showLogTimestamp = logger.showTimestamp
-        delete logger.showTimestamp
-      }
-      delete logger.mute
-      delete logger.dateFormat
-
-      if (!Object.keys(logger).length) {
-        delete config['adapt-authoring-logger']
-      }
-    })
+    .replace('levels', 'adapt-authoring-core', 'logLevels')
+    .replace('showTimestamp', 'adapt-authoring-core', 'showLogTimestamp')
+    .remove('mute', 'dateFormat')
 }
